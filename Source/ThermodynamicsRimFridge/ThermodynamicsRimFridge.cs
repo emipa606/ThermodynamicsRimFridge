0277e5cdd2ec31dc8384c9ea323f25ee3c7fcccc
emipa606 <emil.offermo@litet.net>
2021-07-29 22:07:26 +0200

    Updated for 1.3, using the forked version of thermodynamics

--- Source/ThermodynamicsRimFridge/ThermodynamicsRimFridge.cs
+++ Source/ThermodynamicsRimFridge/ThermodynamicsRimFridge.cs
@@ -1,15 +1,13 @@
-﻿using DThermodynamicsCore.Comps;
+﻿using System.Reflection;
+using DThermodynamicsCore.Comps;
 using HarmonyLib;
 using RimFridge;
-using System.Collections.Generic;
-using System.Reflection;
 using Verse;
 
 namespace ThermodynamicsRimFridge
 {
-
     [StaticConstructorOnStartup]
-    static class ThermodynamicsRimFridge
+    internal static class ThermodynamicsRimFridge
     {
         static ThermodynamicsRimFridge()
         {
@@ -17,29 +15,30 @@ namespace ThermodynamicsRimFridge
             harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
 
-        [HarmonyPatch(typeof(CompDTemperature))]
-        [HarmonyPatch("AmbientTemperature",MethodType.Getter)]
+        [HarmonyPatch(typeof(CompDTemperature), "AmbientTemperature", MethodType.Getter)]
         public class Prefix_IncidentWorker_VisitorGroup
         {
-            [HarmonyPrefix]
             public static bool Prefix(ref CompDTemperature __instance, ref double __result)
             {
                 if (!__instance.parent.Spawned)
                 {
                     return true;
                 }
-                List<Thing> thingList = __instance.parent.PositionHeld.GetThingList(__instance.parent.MapHeld);
-                foreach (Thing thing in thingList)
+
+                var thingList = __instance.parent.PositionHeld.GetThingList(__instance.parent.MapHeld);
+                foreach (var thing in thingList)
                 {
                     var compRefrigerator = thing.TryGetComp<CompRefrigerator>();
                     if (compRefrigerator == null)
                     {
                         continue;
                     }
+
                     __result = compRefrigerator.currentTemp;
                     //Log.Message($"Current temp: {__result}");
                     return false;
                 }
+
                 return true;
             }
         }
